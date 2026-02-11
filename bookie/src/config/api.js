e2dce7027244534b23cb0087676bb0db846fe1cc
GamotechSofta <gamotechsoftware@gmail.com>
2026-02-11 14:52:26 +0530

    3rd commit

--- bookie/src/config/api.js
+++ bookie/src/config/api.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://localhost:5050/api';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5050/api';
 const BASE_URL = API_BASE_URL;
 export default BASE_URL;
